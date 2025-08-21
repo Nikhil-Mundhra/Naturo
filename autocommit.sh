ec225962776285c90bccbe6af5ca3b6bff29b94a
Nikhil Mundhra <nikhilmundhra28@gmail.com>
2025-08-22 00:42:42 +0530

    Auto commit on Fri Aug 22 00:42:42 IST 2025

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-git add .
-git commit -m "Auto commit on $(date)"
-git push
+# Auto commit + push every 5 minutes until stopped (Ctrl + C)
+
+while true; do
+  git add .
+  git commit -m "Auto commit on $(date)"
+  git push
+  echo "✅ Auto commit done at $(date)"
+  sleep 3600   # wait 1 hour
+done
 
 # bash autocommit.sh
+
+# to run it forever
+# nohup bash autocommit.sh &
+
+# to stop it later
+# ps aux | grep autocommit
+# kill <PID>
